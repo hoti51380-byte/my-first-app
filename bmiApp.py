49580390e87366d2b4c3902f085f01f62331616a
hoti51380-byte <hoti51380@gmail.com>
2026-08-11 14:27:37 +0700

    Create bmiApp.py

---
+++ bmiApp.py
@@ -0,0 +1,26 @@
+import streamlit as at
+
+st.markdown ("# :red[❤️ คำนวนดัชนีมวลกาย BMI]")
+st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
+
+weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+
+if st.button("คำนวนค่า BMI 🔍"):
+   height_m = height_cm / 10
+   bmi = weight / (height_m ** 2)
+   
+   st.write("---")
+   st.header(f"ค่า BMI ของคุณคือ: **{bmi: .2f}**")
+   
+   if bmi < 18.5:
+      st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+   elif 18.5 <= bmi < 23.0
+      st.success("⭐ คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+   elif 23.0 <=bmi < 25.0:
+      st.info("🩺 คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
+   else:
+      st.error("️‍🔥 คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+
+st.divider()
+st.write("นางสาวปิโยรส แย้มเอิบสิน เลขที่38 ม.4/9")
