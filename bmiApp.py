e6f89f35340a2d85aa0799d0c2eaa8a6c4347142
hoti51380-byte <hoti51380@gmail.com>
2026-08-25 14:08:59 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,6 +1,5 @@
 import streamlit as at
 
-st.markdown("# :red[ คำนวนค่าดัชนีมวลกาย BMI]")
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
 
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
