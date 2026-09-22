4b60839a8c1342e0f72f784e3445da4c5672174a
hoti51380-byte <hoti51380@gmail.com>
2026-09-22 14:00:47 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาว ปิโยรส แย้มเอิบสิน เลขที่ 38 ม.4/9")
